5d18d8fca89f06cadb50ddfcc65c28bcccfc3245
Yoichi Kawasaki <yoichika@microsoft.com>
2018-08-10 07:55:53 +0900

    removed uncesarry lines

--- scripts/mysql-helpers/check-replication-loop.sh
+++ scripts/mysql-helpers/check-replication-loop.sh
@@ -4,5 +4,3 @@ set -e -x
 
 kubectl run mysql-client-loop --image=mysql:5.7 -i -t --rm --restart=Never --\
     bash -ic "while sleep 1; do for s in \$(seq 2);do echo -n \"mysql-\$s.mysql\"; mysql -h mysql-\$s.mysql -e 'SHOW SLAVE STATUS\G' | grep Read_Master_Log_Pos;done; done"
-
-#    bash -ic "while sleep 1; do for s in \$(seq 2);do mysql -h mysql-\$s.mysql -e 'SHOW SLAVE STATUS\G';done; done"
