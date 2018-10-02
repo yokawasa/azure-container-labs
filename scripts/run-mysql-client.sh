042a83b97abe7a47695dcc1007a685254868a97c
Yoichi Kawasaki <yoichika@microsoft.com>
2018-10-02 17:28:09 +0900

    Added helper scripts

---
+++ scripts/run-mysql-client.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+MYSQL_USER="<mysql-user>"
+MYSQL_PASSWORD="<mysql-password>"
+MYSQL_HOST="<mysql-host>"
+MYSQL_DB="<mysql-db>"
+kubectl run -it --rm --image=mysql:5.7 mysql-client -- mysql -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASSWORD $MYSQL_DB
